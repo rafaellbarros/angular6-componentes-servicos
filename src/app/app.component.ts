ac2e0aab4be44bea64a4da6740b412ba51efcc3c
Rafael Barros <rafaelbarros.df@gmail.com>
2018-10-25 20:39:19 -0300

    Estrutura do projeto inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+class Task {
+  id: number;
+  name: string;
+}
+
+const TASKS: Task[] = [
+  {id: 1, name: 'Trabalhar'},
+  {id: 2, name: 'Lavar pratos'},
+  {id: 3, name: 'Tirar poeira'},
+  {id: 4, name: 'Comprar no supermecado'},
+  {id: 5, name: 'Cuidar das crianças'},
+  {id: 6, name: 'Jogar videogame'},
+  {id: 7, name: 'Almoçar'},
+  {id: 8, name: 'Jantar'},
+  {id: 9, name: 'Fazer exercício'},
+  {id: 10, name: 'Pagar contas'}
+];
+
+@Component({
+  selector: 'app-task-edit',
+  template: `
+  <div *ngIf="task">
+    <input type="text" [(ngModel)]="task.name"/>
+  </div>
+  `
+})
+
+export class TaskEditComponent {
+  @Input()
+  task: Task;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'angular6-componentes-servicos';
+  title = 'Lista de Tarefas';
+  tasks: Task[] = TASKS;
+  selectedTask: Task;
+
+  onClick(task) {
+    console.log(task);
+    this.selectedTask = task;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent, TaskEditComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
