b6647c96e5c7f64aea4b7b317a91ad21ce8d2306
Rafael Barros <rafaelbarros.df@gmail.com>
2018-10-27 19:56:56 -0300

    Criando servico para administrar as tarefas

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,18 +1,6 @@
+import { TaskService } from './../task.service';
 import { Component, OnInit } from '@angular/core';
-import { Task } from 'src/app/task/task.model';
-
-const TASKS: Task[] = [
-  {id: 1, name: 'Trabalhar'},
-  {id: 2, name: 'Lavar pratos'},
-  {id: 3, name: 'Tirar poeira'},
-  {id: 4, name: 'Comprar no supermecado'},
-  {id: 5, name: 'Cuidar das crianças'},
-  {id: 6, name: 'Jogar videogame'},
-  {id: 7, name: 'Almoçar'},
-  {id: 8, name: 'Jantar'},
-  {id: 9, name: 'Fazer exercício'},
-  {id: 10, name: 'Pagar contas'}
-];
+import { Task } from '../task/task.model';
 
 @Component({
   selector: 'task-list',
@@ -22,12 +10,13 @@ const TASKS: Task[] = [
 export class TaskListComponent implements OnInit {
 
   title = 'Minhas Tarefas';
-  tasks: Task[] = TASKS;
+  tasks: Task[];
   selectedTask: Task;
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.tasks = this.taskService.getTasks();
   }
 
   selectTask(task) {
