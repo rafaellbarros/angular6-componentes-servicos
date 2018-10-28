c397f50a5b8814a16ce6934ec83a2bbde8bba1ee
Rafael Barros <rafaelbarros.df@gmail.com>
2018-10-27 22:22:00 -0300

    Criando rota para edição de tarefas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { TaskEditComponent } from './task/task-edit.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tasks/list', pathMatch: 'full'},
-  { path: 'tasks/list', component: TaskListComponent }
+  { path: 'tasks/list', component: TaskListComponent },
+  { path: 'tasks/:id/edit', component: TaskEditComponent }
 ];
 
 @NgModule({
